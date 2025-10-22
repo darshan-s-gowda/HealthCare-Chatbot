fad6f39e0e49eae91cf3a554679f9b8166c11236
darshan-s-gowda <darshandarshu1115@gmail.com>
2025-10-22 13:30:56 +0530

    work updated

--- test_rag.py
+++ test_rag.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
 from src.prompt import *
 import os
-
-app = Flask(__name__)
+from dotenv import load_dotenv
 
 load_dotenv()
+
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -19,7 +17,6 @@ os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 embeddings = download_hugging_face_embeddings()
-
 index_name = "medical-chatbot"
 
 docsearch = PineconeVectorStore.from_existing_index(
@@ -38,20 +35,5 @@ prompt = ChatPromptTemplate.from_messages([
 question_answer_chain = create_stuff_documents_chain(chatmodel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
-
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    print("User Input:", msg)
-    response = rag_chain.invoke({"input": msg})
-    print("Response:", response["answer"])
-    return str(response["answer"])
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+response = rag_chain.invoke({"input": "What is acromegaly?"})
+print("Response:", response)
